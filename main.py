e32d7a4a5ac1b0d79407db676c21a44c0d8dbb56
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-07-02 22:30:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ def get_args():
     parser.add_argument('-f', '--file', type=str, help='path file')
     parser.add_argument('-s', '--save', type=str, help='path save file')
     parser.add_argument('--type', type=str, default='LZ77', help='name data compression algorithm')
-    parser.add_argument('--compress', help='compression mode', action="store_true")
-    parser.add_argument('--max_length', type=int, default=255, help='max length pattern')
-    parser.add_argument('--max_offset', type=int, default=15, help='max offset in buffer')
+    parser.add_argument('-c', '--compress', help='compression mode', action="store_true")
+    parser.add_argument('-d', '--decompress', help='decompression mode', action="store_true")
+    parser.add_argument('--max_length', type=int, default=15, help='max length pattern')
+    parser.add_argument('--max_offset', type=int, default=255, help='max offset in buffer')
 
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     opt = get_args()
-    tool = LZ77(max_length=opt.max_length, max_offset=opt.max_offset)
+    tool = LZ77(max_offset=opt.max_offset, max_length=opt.max_length)
 
-    type_mode = tool.encode if opt.compress else tool.decode
-    type_mode(opt.file, opt.save)
+    type_mode = tool.compress if opt.compress else None
+    type_mode = tool.decompress if opt.decompress else None
+    if type_mode not is None:
+        type_mode(path_file=opt.file, path_save=opt.save)
     
