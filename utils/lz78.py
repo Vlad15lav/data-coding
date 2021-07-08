83ff3923099bd047115f13e1baf1ba874ad6167b
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-07-08 23:20:32 +0300

    Update lz78.py

--- utils/lz78.py
+++ utils/lz78.py
@@ -10,7 +10,7 @@ class LZ78:
         history = dict()
         out = bitarray(endian='big')
 
-        last_idx, next_idx, bit_numeric, last_byte = 0, 1, 1, True
+        last_idx, next_idx, bit_numeric, is_end_mark = 0, 1, 1, True
 
         while message:
             number_size = math.ceil(math.log(bit_numeric, 2))
@@ -18,7 +18,7 @@ class LZ78:
 
             if not byte:
                 if last_idx != 0:
-                    last_byte = False
+                    is_end_mark = False
 
                     num = bin(last_idx)[2:].zfill(number_size)
                     out.extend(num)
@@ -42,4 +42,41 @@ class LZ78:
         file.close()
 
     def decompress(self, path_file: str, path_save: str):
-        pass
+        output_stream = open(path_save, "wb")
+
+        data = bitarray(endian='big')
+        with open(path_file,'rb') as f:
+            data.fromfile(f)
+
+        history = dict()
+        bit_numeric, number_size, is_end_mark = 1, 1, data[-1]
+        data = data[:-1]
+
+        pos = 0
+        iteration = 1
+
+        while pos + number_size + 8 < len(data):
+            current_idx = data[pos:pos + number_size].to01()
+            current_idx = int(current_idx, 2)
+
+            pos += number_size
+
+            byte = data[pos:pos+8].tobytes()
+            
+            pos += 8
+            bit_numeric += 1
+            number_size = math.ceil(math.log(bit_numeric, 2))
+
+            if current_idx != 0:
+                history_byte = history[current_idx]
+
+                if pos + 8 >= len(data) and not is_end_mark:
+                    byte = history_byte
+                else:
+                    byte = history_byte + byte
+
+            history[iteration] = byte
+            output_stream.write(byte)
+            iteration += 1
+        
+        output_stream.close()
